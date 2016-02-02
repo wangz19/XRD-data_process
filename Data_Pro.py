fd76fbebadf21447fbd8e8ebacd5f2b550c33679
wangz19 <zehaiwang@x-ray-185.dynamic2.rpi.edu>
2016-02-02 12:07:49 -0500

    First commit

---
+++ Data_Pro.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#input variables
+data_directory= "/Users/zehaiwang/Documents/Research_Progress/XRD/20160107/xy_data/" # directory of xy data file
+filename="UnT_1.xy"
+
+txt=open(data_directory+filename)
+print "%r successfully opened"%filename
